a46b5e625c14c53b70542d5b7188f2113054ca90
Rafael Borges <raf.vbs90@gmail.com>
2023-07-13 10:51:46 -0300

    initial commit

---
+++ src/test/java/br/cborg/templateapi/TemplateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cborg.templateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
